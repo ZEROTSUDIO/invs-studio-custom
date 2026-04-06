e52310982593bd91994d8848a9c87dcbe50d0df2
ZEROTSUDIO <zerotsustudios341@gmail.com>
2026-04-06 13:51:19 +0700

    changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,21 +56,21 @@ $route['login'] = 'login';
 //route untuk dashboard
 $route['dashboard'] = 'dashboard';
 //route untuk blog
-$route['blog'] = 'welcome/blog';
-$route['blog/(:num)'] = 'welcome/blog/$1';
-//route untuk portofolio
-$route['portofolio'] = 'welcome/portofolio';
-$route['portofolio/(:num)'] = 'welcome/portofolio/$1';
-$route['portofolio/(:any)'] = 'welcome/single2/$1';
-//route untuk halaman kategori artikel
-$route['kategori/(:any)'] = 'welcome/kategori/$1';
-$route['kategori/(:any)/(:num)'] = 'welcome/kategori/$1/$s2';
-//route untuk halaman cari artikel
-$route['search'] = 'welcome/search';
-$route['search/(:any)'] = 'welcome/search/$1';
-$route['search/(:any)/(:num)'] = 'welcome/search/$1/$2';
-//route untuk halaman page
-$route['page/(:any)'] = 'welcome/page/$1';
+// $route['blog'] = 'welcome/blog';
+// $route['blog/(:num)'] = 'welcome/blog/$1';
+// //route untuk portofolio
+// $route['portofolio'] = 'welcome/portofolio';
+// $route['portofolio/(:num)'] = 'welcome/portofolio/$1';
+// $route['portofolio/(:any)'] = 'welcome/single2/$1';
+// //route untuk halaman kategori artikel
+// $route['kategori/(:any)'] = 'welcome/kategori/$1';
+// $route['kategori/(:any)/(:num)'] = 'welcome/kategori/$1/$s2';
+// //route untuk halaman cari artikel
+// $route['search'] = 'welcome/search';
+// $route['search/(:any)'] = 'welcome/search/$1';
+// $route['search/(:any)/(:num)'] = 'welcome/search/$1/$2';
+// //route untuk halaman page
+// $route['page/(:any)'] = 'welcome/page/$1';
 //route untuk URL SEO artikel
 $route['migrate'] = 'migrate';
 $route['(:any)'] = 'welcome/single/$1';
